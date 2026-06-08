4b121e023888bbbf082272685f8adcf0876d0a87
megazoid <megazoid@protonmail.com>
2026-06-08 11:23:20 +0300

    remove used imports

--- src/test/java/org/acme/QuizWordServiceIT.java
+++ src/test/java/org/acme/QuizWordServiceIT.java
@@ -12,8 +12,6 @@ import org.acme.service.QuizWordService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 
